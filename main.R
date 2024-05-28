0a6da03cedbe03f61712ce04f84ab815dea617dc
ChangjunLee <changjun.chris.lee@gmail.com>
2024-05-28 14:13:09 +0900

    The first file upload

---
+++ main.R
@@ -0,0 +1,33 @@
+# install.packages("usethis")
+# library(usethis)
+# # library(usethis)
+# 
+# use_git_config(
+#   user.name = "ChangjunLee", 
+#   user.email = "changjun.chris.lee@gmail.com"
+# )
+
+# usethis::create_github_token()
+# 
+# gitcreds::gitcreds_set()
+# 
+# 
+# git_vaccinate()
+
+
+
+usethis::use_git()
+
+
+usethis::use_github()
+
+
+
+# add some codes like this
+
+library(tidyverse)
+mtcars %>% head
+
+mtcars %>% ggplot(aes(x = cyl,
+                      y = hp)) +
+  geom_boxplot()
